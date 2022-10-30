93c6888cf225660f6b41db4d487e45ee843d9028
thebinij <binijshrestha@gmail.com>
2022-10-30 15:01:37 +0000

    refactor: adding guard clause

--- element.sh
+++ element.sh
@@ -2,9 +2,11 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
-if [[ $1 ]]
+if [[ -z $1 ]]
 then
-  # if argument is number
+  echo "Please provide an element as an argument."
+else
+  # check if argument is number
   if [[ $1 =~ ^[0-9]+$ ]]
   then
     ELEMENT_INFO=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1")
@@ -14,13 +16,11 @@ then
   # if element not found
   if [[ -z $ELEMENT_INFO ]]
   then
-    echo -e "\nI could not find that element in the database\n"
+    echo "I could not find that element in the database."
   else 
     echo "$ELEMENT_INFO" | while read TYPE_ID BAR ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE
     do
-    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius.\n"
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     done
   fi
-else
-  echo "Please provide an element as an argument."
 fi
