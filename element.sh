ee7b8d5b2e363bb464fb855132201181b65b2d12
thebinij <binijshrestha@gmail.com>
2022-10-30 14:25:54 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[ $1 ]]
+then
+  # if argument is number
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT_INFO=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) WHERE atomic_number = $1")
+  else
+    ELEMENT_INFO=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) WHERE symbol = '$1' OR name = '$1'")
+  fi
+  # if element not found
+  if [[ -z $ELEMENT_INFO ]]
+  then
+    echo -e "\nI could not find that element in the database\n"
+  else 
+    echo "$ELEMENT_INFO" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
+    do
+    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius.\n"
+    done
+  fi
+else
+  echo "Please provide an element as an argument."
+fi
